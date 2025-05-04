9a5976d876eed8c076407a85ce664cc7ea733013
Scrooge86x <53798834+Scrooge86x@users.noreply.github.com>
2025-05-04 17:37:45 +0200

    Removed unused ExperimentalMaterial3Api OptIn from PasswordInput.

--- app/src/main/java/com/example/chore_buddy/components/PasswordInput.kt
+++ app/src/main/java/com/example/chore_buddy/components/PasswordInput.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PasswordInput(
     value: String,
