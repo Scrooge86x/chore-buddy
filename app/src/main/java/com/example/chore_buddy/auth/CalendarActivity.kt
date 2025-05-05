cb80af1ca6049240db16d708fa3c33123df06581
Scrooge86x <53798834+Scrooge86x@users.noreply.github.com>
2025-05-05 21:40:22 +0200

    Added placeholder CalendarActivity. Useful for testing logging in.

---
+++ app/src/main/java/com/example/chore_buddy/auth/CalendarActivity.kt
@@ -0,0 +1,77 @@
+package com.example.chore_buddy.auth
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.chore_buddy.R
+import com.example.chore_buddy.ui.theme.ChorebuddyTheme
+
+class CalendarActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ChorebuddyTheme {
+                CalendarScreen()
+            }
+        }
+    }
+}
+
+@Preview(apiLevel = 34)
+@Composable
+fun CalendarScreen() {
+    val interFontFamily = FontFamily(
+        Font(R.font.inter_regular),
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = "Successfully Logged In",
+            color = Color.White,
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 64.dp),
+
+            style = TextStyle(
+                fontFamily = interFontFamily
+            )
+        )
+        Text(
+            text = "Calendar Activity",
+            color = Color.White,
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 64.dp),
+
+            style = TextStyle(
+                fontFamily = interFontFamily
+            )
+        )
+    }
+}
