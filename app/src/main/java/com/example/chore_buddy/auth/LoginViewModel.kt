35a782813e87090383b96d653ba5506629eeada0
MrAlantari <piorko050@gmail.com>
2025-05-06 21:38:52 +0200

    Added LoginViewModel class to provide interaction between AuthRepository and LoginActivity.

---
+++ app/src/main/java/com/example/chore_buddy/auth/LoginViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.chore_buddy.auth
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class LoginViewModel() : ViewModel() {
+    var email by mutableStateOf("")
+    var password by mutableStateOf("")
+    var isLoading by mutableStateOf(false)
+    var loginError by mutableStateOf<String?>(null)
+    var loginSuccess by mutableStateOf(false)
+
+    fun signIn() {
+        loginError = null
+        loginSuccess = false
+        isLoading = true
+
+        viewModelScope.launch {
+            when (val result = AuthRepository.signInWithEmailAndPassword(email, password)) {
+                is AuthRepository.Result.Success -> {
+                    loginSuccess = true
+                }
+                is AuthRepository.Result.Error -> {
+                    loginError = when (result.exception) {
+                        is com.google.firebase.auth.FirebaseAuthInvalidUserException -> "Użytkownik nie istnieje lub jest zablokowany."
+                        is com.google.firebase.auth.FirebaseAuthInvalidCredentialsException -> "Nieprawidłowy email lub hasło."
+                        else -> result.exception.message ?: "Wystąpił nieznany błąd logowania."
+                    }
+                }
+            }
+            isLoading = false
+        }
+    }
+}
