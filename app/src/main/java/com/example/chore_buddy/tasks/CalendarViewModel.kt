ca593e5b9f5c325a438e447242457493030c4772
MrAlantari <piorko050@gmail.com>
2025-05-25 22:34:15 +0200

    Implemented variables and checkIfInGroup function to handle temporary button from CalendarActivity.

--- app/src/main/java/com/example/chore_buddy/tasks/CalendarViewModel.kt
+++ app/src/main/java/com/example/chore_buddy/tasks/CalendarViewModel.kt
@@ -1,7 +1,57 @@
 package com.example.chore_buddy.tasks
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.chore_buddy.auth.AuthRepository
+import com.example.chore_buddy.users.UserRepository
+import kotlinx.coroutines.launch
 
 class CalendarViewModel(): ViewModel() {
 
+    var isInGroup by mutableStateOf<Boolean>(false)
+        private set
+
+    var groupId by mutableStateOf<String?>(null)
+        private set
+
+    var isLoading by mutableStateOf(false)
+        private set
+
+    var errorMessage by mutableStateOf<String?>(null)
+        private set
+
+    init {
+        checkIfInGroup()
+    }
+
+    fun checkIfInGroup() {
+        viewModelScope.launch {
+            isLoading = true
+            errorMessage = null
+
+            try {
+                when (val result =
+                          UserRepository.getUserByUid(AuthRepository.getCurrentUser()!!.uid)) {
+                    is UserRepository.UserResult.Success -> {
+                        if (!result.data?.groupId.isNullOrBlank()) {
+                            isInGroup = true
+                            groupId = result.data?.groupId
+                        }
+                    }
+                    is UserRepository.UserResult.Error -> {
+                        errorMessage = result.exception.message ?: "Unknown error occurred."
+                    }
+                }
+            } catch (e: Exception) {
+                errorMessage = e.message ?: "Unknown error occurred."
+            }
+        }
+    }
+
+    fun resetError() {
+        errorMessage = null
+    }
 }
