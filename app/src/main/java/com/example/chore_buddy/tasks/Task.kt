403d9a9af90e7d14476e6ed40eda0b396950b513
Scrooge86x <53798834+Scrooge86x@users.noreply.github.com>
2025-06-05 17:36:56 +0200

    Updated Task data class. Added a new field assignedToName and renamed assignedTo to assignedToId. Added a missing @DocumentId. These changes will help with querying, for example in DayInfoActivity every task will display the assigned user name on it, so without storing the name in Task it would require multiple queries to the database (firestore charges per query count not size).

--- app/src/main/java/com/example/chore_buddy/tasks/Task.kt
+++ app/src/main/java/com/example/chore_buddy/tasks/Task.kt
@@ -1,11 +1,14 @@
 package com.example.chore_buddy.tasks
 
 import com.google.firebase.Timestamp
+import com.google.firebase.firestore.DocumentId
 
 data class Task(
+    @DocumentId
     var title: String = "",
     var description: String = "",
-    var assignedTo: String = "",
+    var assignedToId: String = "",
+    var assignedToName: String = "",
     var createdBy: String = "",
     var groupId: String = "",
     var status: Boolean = true,
