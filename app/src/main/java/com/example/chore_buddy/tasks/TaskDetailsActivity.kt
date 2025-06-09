cae405250090e0ddb5e606d08676b2edb6b79938
MrAlantari <piorko050@gmail.com>
2025-06-10 00:58:19 +0200

    Implemented TaskDetailsViewModel with various changes in TaskDetailsActivity. There was no testing. GLHF

--- app/src/main/java/com/example/chore_buddy/tasks/TaskDetailsActivity.kt
+++ app/src/main/java/com/example/chore_buddy/tasks/TaskDetailsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.chore_buddy.tasks
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -18,12 +20,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.chore_buddy.R
 import com.example.chore_buddy.components.CustomButton
 import com.example.chore_buddy.components.Logo
@@ -48,6 +52,89 @@ class TaskDetailsActivity : ComponentActivity() {
 }
 
 val interFontFamily = FontFamily(Font(R.font.inter_regular))
+
+@Composable
+fun TaskDetailsScreen(
+    taskId: String
+) {
+    val taskDetailsViewModel: TaskDetailsViewModel = viewModel()
+
+    val context = LocalContext.current
+
+    LaunchedEffect(taskDetailsViewModel.errorMessage) {
+        taskDetailsViewModel.errorMessage?.let {
+            Toast.makeText(context, it, Toast.LENGTH_LONG).show()
+            taskDetailsViewModel.resetError()
+        }
+    }
+
+    taskDetailsViewModel.getTask(taskId)
+
+    val task = taskDetailsViewModel.task ?: Task(
+        title = "For unknown reason there is no task."
+    )
+
+    taskDetailsViewModel.checkIfIsAdminOrSelf()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorScheme.background)
+            .verticalScroll(rememberScrollState())
+            .imePadding()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Logo()
+        ScreenHeading(text = task.title)
+        Spacer(modifier = Modifier.height(72.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.Top
+        ) {
+            Column {
+                DateView(
+                    label = "Creation Date",
+                    date = task.createdAt.toDate().toString(),
+                )
+                DateView(
+                    label = "Due Date",
+                    date = task.dueDate.toDate().toString(),
+                )
+            }
+            StatusCheckbox(
+                isChecked = task.status,
+                isAdmin = taskDetailsViewModel.isAdminOrSelf,
+                onClick = { taskDetailsViewModel.changeIsChecked(task.id) }
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+            .background(colorScheme.onBackground)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        MultiLineInput(
+            label = "Task Description",
+            value = task.description,
+            onValueChange = { if (taskDetailsViewModel.isAdminOrSelf) task.description = it },
+            placeholderText = "Task details...",
+            modifier = Modifier.padding(horizontal = 4.dp),
+            isEnabled = taskDetailsViewModel.isAdminOrSelf,
+        )
+        if (taskDetailsViewModel.isAdminOrSelf) {
+            Spacer(modifier = Modifier.height(48.dp))
+            CustomButton(
+                text = "SAVE",
+                onClick = {
+                    taskDetailsViewModel.updateDescription(task.id)
+                }
+            )
+        }
+    }
+}
 
 @Composable
 fun DateView(
@@ -112,77 +199,6 @@ fun StatusCheckbox(
                 isAdmin -> IconButton(onClick = onClick) {}
                 isChecked -> checkedIcon()
             }
-        }
-    }
-}
-
-@Composable
-fun TaskDetailsScreen(
-    taskId: String,
-    isAdmin: Boolean = false
-) {
-    var isChecked by remember { mutableStateOf(false) }
-    var description by remember { mutableStateOf("") }
-
-    val creationDate = "01/01/2000"
-    val dueDate = "01/01/2001"
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(colorScheme.background)
-            .verticalScroll(rememberScrollState())
-            .imePadding()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Logo()
-        ScreenHeading(text = "Task Title")
-        Spacer(modifier = Modifier.height(72.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.Top
-        ) {
-            Column {
-                DateView(
-                    label = "Creation Date",
-                    date = creationDate,
-                )
-                DateView(
-                    label = "Due Date",
-                    date = dueDate,
-                )
-            }
-            StatusCheckbox(
-                isChecked = isChecked,
-                isAdmin = isAdmin,
-                onClick = { isChecked = !isChecked }
-            )
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Box(modifier = Modifier
-            .fillMaxWidth()
-            .height(1.dp)
-            .background(colorScheme.onBackground)
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        MultiLineInput(
-            label = "Task Description",
-            value = description,
-            onValueChange = { if (isAdmin) description = it },
-            placeholderText = "Task details...",
-            modifier = Modifier.padding(horizontal = 4.dp),
-            isEnabled = isAdmin,
-        )
-        if (isAdmin) {
-            Spacer(modifier = Modifier.height(48.dp))
-            CustomButton(
-                text = "SAVE",
-                onClick = {
-                    // save logic here
-                }
-            )
         }
     }
 }
